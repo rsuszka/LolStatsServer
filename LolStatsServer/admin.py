dfda6b99c817eaf15c14bd3f198cc0190955d19e
Rafał Suszka <suszkarafal@gmail.com>
2019-12-11 16:30:59 +0100

    Add databases classes

--- LolStatsServer/admin.py
+++ LolStatsServer/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
-from .models import Champion
-from .models import RiotApiKey
+from .models import Champion, RiotApiKey, Match, MatchBan, MatchChampion
 
-admin.site.register(Champion)
-admin.site.register(RiotApiKey)
+
+class ChampionAdmin(admin.ModelAdmin):
+    list_display = ["id", "champion_id", "name"]
+
+
+class RiotApiKeyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in RiotApiKey._meta.get_fields()]
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ["id", "game_id", "queue_id", "game_version", "duration"]
+
+
+class MatchBanAdmin(admin.ModelAdmin):
+    list_display = ["id", "match_id", "champion_id"]
+
+
+class MatchChampionAdmin(admin.ModelAdmin):
+    list_display = ["id", "match_id", "champion_id", "kills", "deaths", "assists", "damage_defeat",
+                    "damage_to_objectives", "damage_taken", "gold_earned", "vision_score", "total_heal",
+                    "champion_level", "first_blood_kill", "first_tower", "lane"]
+
+
+admin.site.register(Champion, ChampionAdmin)
+admin.site.register(RiotApiKey, RiotApiKeyAdmin)
+admin.site.register(Match, MatchAdmin)
+admin.site.register(MatchBan, MatchBanAdmin)
+admin.site.register(MatchChampion)
--- LolStatsServer/models.py
+++ LolStatsServer/models.py
@@ -9,9 +9,6 @@ class Champion(models.Model):
         verbose_name = "Champion"
         verbose_name_plural = "Champions"
 
-    def __str__(self):
-        return self.name
-
 
 class RiotApiKey(models.Model):
     api_key = models.CharField(max_length=50)
@@ -20,5 +17,44 @@ class RiotApiKey(models.Model):
         verbose_name = "RiotApiKey"
         verbose_name_plural = "RiotApiKeys"
 
-    def __str__(self):
-        return self.api_key
+
+class Match(models.Model):
+    game_id = models.IntegerField()
+    queue_id = models.IntegerField()
+    game_version = models.IntegerField()
+    duration = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Match"
+        verbose_name_plural = "Matches"
+
+
+class MatchBan(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    champion = models.ForeignKey(Champion, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "MatchBan"
+        verbose_name_plural = "MatchBans"
+
+
+class MatchChampion(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    champion = models.ForeignKey(Champion, on_delete=models.CASCADE)
+    kills = models.IntegerField()
+    deaths = models.IntegerField()
+    assists = models.IntegerField()
+    damage_defeat = models.IntegerField()
+    damage_to_objectives = models.IntegerField()
+    damage_taken = models.IntegerField()
+    gold_earned = models.IntegerField()
+    vision_score = models.IntegerField()
+    total_heal = models.IntegerField()
+    champion_level = models.IntegerField()
+    first_blood_kill = models.BooleanField()
+    first_tower = models.BooleanField()
+    lane = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "MatchChampion"
+        verbose_name_plural = "MatchChampions"
